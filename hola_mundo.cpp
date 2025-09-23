fc6af7069efd81c1711a92802707581104c42ea8
Sebascoga22 <sebascoga2030@gmail.com>
2025-09-23 17:53:39 -0400

    feat: add more messages to the program

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
     cout << "Mi nombre es: [Sebastian Cohen]" << endl;
+    cout << "Estoy aprendiendo Git!" << endl;
+    cout << "Este es mi segundo commit!" << endl;
     return 0;
 }
