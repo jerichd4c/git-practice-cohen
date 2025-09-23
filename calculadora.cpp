88727d61ad99dae7eead38ec7c4d2cbd28a866bb
Sebascoga22 <sebascoga2030@gmail.com>
2025-09-23 18:24:33 -0400

    feat: add basic calculator

---
+++ calculadora.cpp
@@ -0,0 +1,12 @@
+// calculadora.cpp
+#include <iostream>
+using namespace std;
+
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "10 + 3 = " << sumar(10, 3) << endl;
+    return 0;
+}
